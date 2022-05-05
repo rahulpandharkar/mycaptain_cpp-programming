6694eea39c60b76c1786a95c7e57be13db9d0a4e
rahulpandharkar <103379268+rahulpandharkar@users.noreply.github.com>
2022-05-05 21:35:39 +0530

    Add files via upload

---
+++ mycaptain_cpp-programming_product_of_two_numbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std; 
+int main()
+{
+	cout<<"Enter two numbers to find their product"<<endl; 
+	double a, b; 
+	cin>>a>>b; 
+	cout<<"Product is: "<<a*b<<endl;
+return 0; 
+}
---
+++ mycaptain_cpp-programming_size_of_datatypes.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std; 
+int main()
+{ 
+cout<<"Size of int: "<<sizeof(int)<<" Byte(s)"<<endl; 
+cout<<"Size of float: "<<sizeof(float)<<" Byte(s)"<<endl; 
+cout<<"Size of double: "<<sizeof(double)<<" Byte(s)"<<endl; 
+cout<<"Size of char: "<<sizeof(char)<<" Byte(s)"<<endl; 
+return 0;
+}
