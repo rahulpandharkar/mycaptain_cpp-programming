7f834e7b085f6a5fe4db61134c95ca8e45ddb4ab
rahulpandharkar <103379268+rahulpandharkar@users.noreply.github.com>
2022-05-05 21:48:33 +0530

    Add files via upload

---
+++ mycaptain_cpp-programming_eligibility-to-vote.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std; 
+int main()
+{ 
+cout<<"Enter your age: "<<endl; 
+int age; 
+cin>>age; 
+if(age>=18)
+cout<<"You are eligibile to vote"<<endl; 
+else
+cout<<"You are not eligible to vote"<<endl; 
+}
